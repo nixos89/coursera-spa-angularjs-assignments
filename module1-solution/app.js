eefb199146ecc67ece210277227a36cdeced4bb1
nixos89 <nixos89@gmail.com>
2019-01-31 00:28:44 +0100

    Second commit

---
+++ module1-solution/app.js
@@ -0,0 +1,39 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("LunchCheck", [])
+    .controller("LunchCheckController", LunchCheckController);
+
+  LunchCheckController.$inject = ["$scope"];
+
+  function LunchCheckController($scope) {
+    $scope.message = "";
+    $scope.lunchMenu = "";
+    $scope.itemArr = [];
+    $scope.onCheck = function() {
+      $scope.itemArr = $scope.lunchMenu.split(",");
+      switch ($scope.itemArr) {
+        case 0:
+          $scope.message = "Please enter data first";
+          break;
+        case $scope.itemArr.length <= 3:
+          $scope.message = "Enjoy!";
+          break;
+        case $scope.itemArr.length > 3:
+          $scope.message = "Too much!";
+          break;
+      }
+    //   if ($scope.itemArr == undefined) {
+    //     console.log("if ($scope.itemArr == undefined)");
+    //     $scope.message = "Please enter data first";
+    //   } else if ($scope.itemArr.length <= 3) {
+    //     console.log("usao sam u if ($scope.itemArr.length <= 3) ");
+    //     $scope.message = "Enjoy!";
+    //   } else if ($scope.itemArr.length > 3) {
+    //     console.log("usao sam u if ($scope.itemArr.length > 3) ");
+    //     $scope.message = "Too much!";
+    //   }
+    };
+  }
+})();
